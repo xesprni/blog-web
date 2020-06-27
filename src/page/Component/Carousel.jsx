59ff7bd8ffd855501322a3462faa539cd61f435a
Miracle <isseidong@outlook.com>
2020-06-27 10:25:21 +0800

    modify style

--- src/page/Component/Carousel.jsx
+++ src/page/Component/Carousel.jsx
@@ -7,10 +7,10 @@ class HeadCarousel extends React.Component{
         return (
             <Carousel autoplay style={{marginBottom:20}}>
                 <div>
-                    <h3>海内存知己，天涯若比邻</h3>
+                    <h3>海内存知己,天涯若比邻</h3>
                 </div>
                 <div>
-                    <h3>闲时与你立黄昏，灶前笑问粥可温</h3>
+                    <h3>闲时与你立黄昏,灶前笑问粥可温</h3>
                 </div>
                 <div>
                     <h3>赌书消得泼茶香,当时只道是寻常</h3>
